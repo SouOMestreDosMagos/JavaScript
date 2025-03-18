9d443359cdd5c189ed58124245ffc2eae0c00efc
SouOMestreDosMagos <enzogonzalesxavier.dev2024@gmail.com>
2025-03-18 12:09:00 -0300

    Meu primeiro projeto em javascript7

--- js/codigos.js
+++ js/codigos.js
@@ -65,181 +65,21 @@ function mostrarDia() {
         resultado.textContent = "Dia inválido!";
     }
 }
-var num1 = -1;
-var num2 = -1;
-var ope = 0;
-var troca = 0;
+function appendToDisplay(value) {
+    const display = document.getElementById("display");
+    display.value += value;
+}
 
-function n0(){
-    let resultado = document.getElementById("resultado");
-    if (troca == 1){
-        num2 = 0;
-        resultado.textContent = num2;
-    }else{
-        num1 = 0;
-        resultado.textContent = num1;
-    }
+function clearDisplay() {
+    const display = document.getElementById("display");
+    display.value = "";
 }
-function n1(){
-    let resultado = document.getElementById("resultado");
-    if (troca == 1){
-        num2 = 1;
-        resultado.textContent = num2;
 
-    }else{
-        num1 = 1;
-        resultado.textContent = num1;
-    }
-}
-function n2(){
-    let resultado = document.getElementById("resultado");
-    if (troca == 1){
-        num2 = 2;
-        resultado.textContent = num2;
-    }else{
-        num1 = 2;
-        resultado.textContent = num1;
-    }
-}
-function n3(){
-    let resultado = document.getElementById("resultado");
-    if (troca == 1){
-        num2 = 3;
-        resultado.textContent = num2;
-    }else{
-        num1 = 3;
-        resultado.textContent = num1;
-    }
-}
-function n4(){
-    let resultado = document.getElementById("resultado");
-    if (troca == 1){
-        num2 = 4;
-        resultado.textContent = num2;
-    }else{
-        num1 = 4;
-        resultado.textContent = num1;
-    }
-}
-function n5(){
-    let resultado = document.getElementById("resultado");
-    if (troca == 1){
-        num2 = 5;
-        resultado.textContent = num2;
-    }else{
-        num1 = 5;
-        resultado.textContent = num1;
-    }
-}
-function n6(){
-    let resultado = document.getElementById("resultado");
-    if (troca == 1){
-        num2 = 6;
-        resultado.textContent = num2;
-    }else{
-        num1 = 6;
-        resultado.textContent = num1;
-    }
-}
-function n7(){
-    let resultado = document.getElementById("resultado");
-    if (troca == 1){
-        num2 = 7;
-        resultado.textContent = num2;
-    }else{
-        num1 = 7;
-        resultado.textContent = num1;
-    }
-}
-function n8(){
-    let resultado = document.getElementById("resultado");
-    if (troca == 1){
-        num2 = 8;
-        resultado.textContent = num2;
-    }else{
-        num1 = 8;
-        resultado.textContent = num1;
-    }
-}
-function n9(){
-    let resultado = document.getElementById("resultado");
-    if (troca == 1){
-        num2 = 9;
-        resultado.textContent = num2;
-    }else{
-        num1 = 9;
-        resultado.textContent = num1;
-    }
-}
-function adicao(){
-    let resultado = document.getElementById("resultado");
-    if (num1 == -1){
-        resultado.textContent = "adicione um numero primeiro";
-    }else{
-        ope = 1;
-        resultado.textContent = "adição";
-        troca = 1;
-    }
-}
-function subtracao(){
-    let resultado = document.getElementById("resultado");
-    if (num1 == -1){
-        resultado.textContent = "adicione um numero primeiro";
-    }else{
-        ope = 2;
-        resultado.textContent = "subtração";
-        troca = 1;
-    }
-}
-function multiplicacao(){
-    let resultado = document.getElementById("resultado");
-    if (num1 == -1){
-        resultado.textContent = "adicione um numero primeiro";
-    }else{
-        ope = 3;
-        resultado.textContent = "multiplicação";
-        troca = 1;
-    }
-}
-function divisao(){
-    let resultado = document.getElementById("resultado");
-    if (num1 == -1){
-        resultado.textContent = "adicione um numero primeiro";
-    }else{
-        ope = 4;
-        resultado.textContent = "divisão";
-        troca = 1;
-    }
-}
-function resolver(){
-    let resultado = document.getElementById("resultado");
-    if (num2 == -1){
-        resultado.textContent = "adicione um segundo numero";
-    }else{
-        if (ope == 1){
-            resultado.textContent = (num1 + num2);
-            num1 = -1;
-            num2 = -1;
-            ope = 0;
-            troca = 0;
-        }else if(ope == 2){
-            resultado.textContent = (num1 - num2);
-            num1 = -1;
-            num2 = -1;
-            ope = 0;
-            troca = 0;
-        }else if(ope == 3){
-            resultado.textContent = (num1 * num2);
-            num1 = -1;
-            num2 = -1;
-            ope = 0;
-            troca = 0;
-        }else if(ope == 4){
-            resultado.textContent = (num1 / num2);
-            num1 = -1;
-            num2 = -1;
-            ope = 0;
-            troca = 0;
-        }  
+function calculateResult() {
+    const display = document.getElementById("display");
+    try {
+        display.value = eval(display.value.replace('×', '*').replace('÷', '/'));
+    } catch {
+        display.value = "Erro";
     }
 }
